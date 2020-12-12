caf7b21d70fd2f18478e807b285cf56b8622b8f7
patriagani <patria.gani@gmail.com>
2020-12-12 23:44:15 +0700

    almost done

---
+++ liff.js
@@ -0,0 +1,67 @@
+window.onload = function() {
+    const useNodeJS = false
+    const defaultLiffId = "1655320023-ZmWOJDBP"
+    let myLiffId = ""
+    myLiffId = defaultLiffId
+    initialLiff(myLiffId)
+}
+
+function initialLiff(myLiffId) {
+    if (!myLiffId) {
+        document.getElementById("loginPage").classList.add('hidden')
+        document.getElementById("errorPage").classList.remove('hidden')
+    } else {
+        createLiff(myLiffId);
+    }
+}
+
+function createLiff(myLiffId) {
+    liff
+        .init({
+            liffId: myLiffId
+        })
+        .then(() => {
+            checkLogin()
+        })
+        .catch((err) => {
+            document.getElementById("loginPage").classList.add('hidden')
+            document.getElementById("errorPage").classList.remove('hidden')
+        })
+}
+
+function checkLogin() {
+    if (liff.isLoggedIn()) {
+        assignProfile()
+        document.getElementById("loginPage").classList.add('hidden')
+        document.getElementById("contentPage").classList.remove('hidden')
+    } else {
+        document.getElementById("loginPage").classList.remove('hidden')
+        document.getElementById("contentPage").classList.add('hidden')
+    }
+}
+
+function assignProfile() {
+    liff.getProfile()
+    .then(profile => {
+        const name = profile.displayName
+        const profilePictureURL = profile.pictureUrl
+        document.getElementById("profilePicture").src = profilePictureURL;
+        document.getElementById("customerGreeting").innerHTML = `Halo ${name}, silahkan memilih menu yang tersedia dibawah ini :`;
+    })
+    .catch((err) => {
+        console.log('error', err);
+    })
+}
+
+function liffLogin() {
+    if (!liff.isLoggedIn()) {
+        liff.login();
+    }
+}
+
+function liffLogout() {
+    if (liff.isLoggedIn()) {
+        liff.logout();
+        window.location.reload();
+    }
+}
---
+++ script.js
@@ -0,0 +1,79 @@
+let totalHarga = 0
+let jumlahMakanan = 0
+let jumlahMinuman = 0
+
+function tambahMenu(harga, tipe, elementID) {
+    totalHarga += harga
+    if(tipe == "makanan") {
+        jumlahMakanan++
+    }
+    else {
+        jumlahMinuman++
+    }
+    let jumlahItem = Number(document.getElementById(elementID).innerHTML)
+    document.getElementById(elementID).innerHTML = `${jumlahItem + 1}`
+    if(jumlahMakanan == 0 && jumlahMinuman == 0) {
+        document.getElementById("foodSummary").innerHTML = `Anda belum memilih menu apapun`
+    }
+    else if(jumlahMakanan == 0 && jumlahMinuman != 0) {
+        document.getElementById("foodSummary").innerHTML = `Total : ${jumlahMinuman} Minuman`
+    }
+    else if(jumlahMinuman == 0 && jumlahMakanan != 0) {
+        document.getElementById("foodSummary").innerHTML = `Total : ${jumlahMakanan} Makanan`
+    }
+    else{
+        document.getElementById("foodSummary").innerHTML = `Total : ${jumlahMakanan} Makanan dan ${jumlahMinuman} Minuman`
+    }
+    document.getElementById("priceSummary").innerHTML = `Harga : Rp ${totalHarga}.000`
+}
+
+function kurangMenu(harga, tipe, elementID) {
+    let jumlahItem = Number(document.getElementById(elementID).innerHTML)
+    if(jumlahItem != 0) {
+        totalHarga -= harga
+        if(tipe == "makanan") {
+            jumlahMakanan--
+        }
+        else {
+            jumlahMinuman--
+        }
+        document.getElementById(elementID).innerHTML = `${jumlahItem - 1}`
+        if(jumlahMakanan == 0 && jumlahMinuman == 0) {
+            document.getElementById("foodSummary").innerHTML = `Anda belum memilih menu apapun`
+        }
+        else if(jumlahMakanan == 0 && jumlahMinuman != 0) {
+            document.getElementById("foodSummary").innerHTML = `Total : ${jumlahMinuman} Minuman`
+        }
+        else if(jumlahMinuman == 0 && jumlahMakanan != 0) {
+            document.getElementById("foodSummary").innerHTML = `Total : ${jumlahMakanan} Makanan`
+        }
+        else{
+            document.getElementById("foodSummary").innerHTML = `Total : ${jumlahMakanan} Makanan dan ${jumlahMinuman} Minuman`
+        }
+        document.getElementById("priceSummary").innerHTML = `Harga : Rp ${totalHarga}.000`
+    }
+}
+
+function pesanMakanan() {
+    if (!liff.isInClient()) {
+        alert("Harap buka aplikasi melalui aplikasi line untuk dapat memesan :)");
+    } else {
+        let text = `
+            Anda telah memesan makanan dengan rincian sebagai berikut :
+            * ${jumlahMakanan} makanan
+            * ${jumlahMinuman} minuman,
+            dengan total harga sebesar : Rp ${totalHarga}.000,-
+
+            Mohon ditunggu ya kak :)
+        `
+
+        liff.sendMessages([{
+            'type': 'text',
+            'text': "Anda telah menggunakan fitur Send Message!"
+        }]).then(function() {
+            window.alert('Pesanan anda telah terkirim, harap menunggu balasan dari admin');
+        }).catch(function(error) {
+            window.alert('Pesanan anda gagal terkirim: ' + error);
+        });
+    }
+}
